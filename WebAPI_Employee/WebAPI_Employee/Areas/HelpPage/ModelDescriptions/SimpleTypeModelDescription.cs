ce10f6a507dc9220b6de93c22b6cdc63708f34c3
William Sorto <william.sorto.alv@gmail.com>
2018-09-23 23:51:04 -0600

    WebAPI
    WebAPI

---
+++ WebAPI_Employee/WebAPI_Employee/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_Employee.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebAPI_Employee/WebAPI_Employee/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAPI_Employee.Startup))]
+
+namespace WebAPI_Employee
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
